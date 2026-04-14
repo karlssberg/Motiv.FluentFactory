09439b56d607c70fe667ef90ef5874310ea095eb
Daniel Karlsson <karlssberg@gmail.com>
2026-04-14 12:30:18 +0100

    test(21-01): add collision and backward-compat test class stubs
    - AccumulatorNameCollisionTests (NAME-04 scaffold)
    - BackwardCompatibilitySnapshotTests (BACK-02 scaffold)

---
+++ src/Converj.Generator.Tests/BackwardCompatibilitySnapshotTests.cs
@@ -0,0 +1,10 @@
+using VerifyCS =
+    Converj.Generator.Tests.CSharpSourceGeneratorVerifier<Converj.Generator.FluentRootGenerator>;
+
+namespace Converj.Generator.Tests;
+
+public class BackwardCompatibilitySnapshotTests
+{
+    [Fact]
+    internal void Placeholder() { /* replaced in later plans */ }
+}
